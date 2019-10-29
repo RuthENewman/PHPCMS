96c318af1420842c4805b3f89fb521917b94595b
Ruth Newman <ruthnewman@gmail.com>
2019-10-29 08:20:35 +0000

    more object-oriented approach to database class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -12,33 +12,40 @@ class Database {
 
     public function openDBConnection() 
     {
-        $this->connection = mysqli_connect(
+        $this->connection = new mysqli(
             DB_HOST,
             DB_USER,
             DB_PASS,
             DB_NAME
         );
 
-        if (mysqli_connect_errno()) {
-            die("Database connection failed " . mysqli_error());
+        if ($this->connection->connect_errno) {
+            die("Database connection failed " . $this->connection->connect_error);
         }
     }
 
     public function query($sql) 
     {
-        return mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+        $this->confirmQuery($result);
+        return $result;
     }
 
     private function confirmQuery($result) 
     {
         if(!$result) {
-            exit("Query failed");
+            exit("Query failed" . $this->connection->error);
         }
     }
 
     public function escapeString($string) 
     {
-        return mysqli_real_escape_string($this->connection, $string);
+        return $this->connection->real_escape_string($string);
+    }
+
+    public function insertID()
+    {
+        return $this->connection->insert_id;
     }
 
 
