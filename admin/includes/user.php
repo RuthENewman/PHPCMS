f9f89d40a06dbe015ed7db8794270852f09d1c7b
Ruth Newman <ruthnewman@gmail.com>
2019-10-30 23:19:40 +0000

    abstracted instantiation method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -26,14 +26,14 @@ class User {
         return $resultSet;
     }
 
-    public static function instantiation($foundUser)
+    public static function instantiation($record)
     {
-        $user = new self();
-        $user->id = $foundUser['id'];
-        $user->email = $foundUser['email'];
-        $user->password = $foundUser['password'];
-        $user->first_name = $foundUser['first_name'];
-        $user->last_name = $foundUser['last_name'];
+        $row = new self();
+        foreach ($record as $attribute => $value) {
+            if($row->has_the_attribute($attrbute)) {
+                $row->attribute = $value;
+            }
+        }
         return $user;
     }
 
