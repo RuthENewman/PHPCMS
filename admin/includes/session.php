7829b58921f1829ce35e69def53eeff07110c147
Ruth Newman <ruthnewman@gmail.com>
2019-11-03 11:01:52 +0000

    adding checking login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,14 +2,25 @@
 
 class Session
 {
-    private $signedIn;
+    private $signedIn = false;
     public $userId;
 
     function __construct()
     {
         session_start();
+        $this->checkLogin();
     }
 
+    private function checkLogin()
+    {
+        if(isset($_SESSION['user_id'])) {
+            $this->userId = $_SESSION['user_id'];
+            $this->signedIn = true;
+        } else {
+            unset($this->userId);
+            $this->signedIn = false;
+        }
+    }
 
 }
 
