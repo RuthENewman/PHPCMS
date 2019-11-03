2bef2b73b5b594bf9c28dc4900dc3c13c733d4cd
Ruth Newman <ruthnewman@gmail.com>
2019-11-03 11:08:17 +0000

    login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -11,6 +11,19 @@ class Session
         $this->checkLogin();
     }
 
+    public function isSignedIn()
+    {
+        return $this->signedIn;
+    }
+
+    public function login($user)
+    {
+       if($user) {
+           $this->userId  = $_SESSION['user_id'] = $user->id;
+           $this->signedIn = true;
+       } 
+    }
+
     private function checkLogin()
     {
         if(isset($_SESSION['user_id'])) {
