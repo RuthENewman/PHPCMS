6e30f56d5f23e9959e64a3070cfb73480c3c911a
Ruth Newman <ruthnewman@gmail.com>
2019-11-05 23:41:25 +0000

    adding set file method with error checking

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,11 @@
 <?php 
+
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+define('DS') ? null : define('SITE_ROOT', DS . 'Applications' . DS . 'XAMPP' . DS . 'xamppfiles' . DS . 'htdocs' . DS . 'my-gallery-app');
+
+define('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT.DS.'admin'.DS.'includes');
+
 require_once("functions.php");
 require_once("new_config.php");
 require_once("database.php");
--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -12,5 +12,42 @@ class Photo extends Model
     public $type;
     public $size;
 
+    public $tmpPath;
+    public $uploadDirectory = "images";
+    public $errors = [];
+    public $uploadErrors = [
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize set",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the max_file_size set.",
+        UPLOAD_ERR_PARTIAL => "The file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded.",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder.",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload."
+    ];
+
+    // Passing $_FILES['uploadedFile'] as an argument
+
+    public function setFile($file)
+    {
+        if(empty($file) || !$file || !is_array($file)) {
+            $this->errors[] = "No file was uploaded here";
+            return false;
+        } elseif ($file['error'] != 0) {
+            $this->errors[] = $this->uploadErrors[$file['error']];
+            return false;
+        } else {
+            $this->filename = basename($file['name']);
+            $this->tmpPath = $file['tmp_name'];
+            $this->type = $file['type'];
+            $this->size = $file['size'];
+        }
+    }
+
+
+
+
+
+    
 
 }
